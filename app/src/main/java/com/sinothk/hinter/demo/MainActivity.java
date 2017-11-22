0276e925cd40b554ff7900538d9aa233f7374b8c
LiangYT <sinothk@126.com>
2017-11-23 01:23:18 +0800

    2017年11月23日 01:23:14

--- app/src/main/java/com/sinothk/hinter/demo/MainActivity.java
+++ app/src/main/java/com/sinothk/hinter/demo/MainActivity.java
@@ -3,11 +3,15 @@ package com.sinothk.hinter.demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import me.leolin.shortcutbadger.ShortcutBadger;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ShortcutBadger.applyCount(this, 23);
     }
 }
