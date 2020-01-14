16b51b0e0a1199fc37950e63313dd4c91e28b9a8
LiangYT <381518188@qq.com>
2020-01-14 17:18:31 +0800

    添加网络权限，适配华为高版本！

--- HinterManager/src/main/java/me/leolin/shortcutbadger/impl/HuaweiHomeBadger.java
+++ HinterManager/src/main/java/me/leolin/shortcutbadger/impl/HuaweiHomeBadger.java
@@ -1,13 +1,9 @@
 package me.leolin.shortcutbadger.impl;
 
 import android.content.ComponentName;
-import android.content.ContentValues;
 import android.content.Context;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 
 import java.util.Arrays;
 import java.util.List;
--- app/src/main/java/com/sinothk/hinter/demo/MainActivity.java
+++ app/src/main/java/com/sinothk/hinter/demo/MainActivity.java
@@ -1,8 +1,9 @@
 package com.sinothk.hinter.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.sinothk.hinter.HinterManager;
 
